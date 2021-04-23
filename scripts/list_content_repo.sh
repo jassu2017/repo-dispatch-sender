274c72a8288457856eabd6f0ae03215caefee1ac
jassu2017 <jayshree.prasad@live.com>
2021-04-23 16:36:55 +0530

    updating a file in other repo using repo dispatch5

--- scripts/list_content_repo.sh
+++ scripts/list_content_repo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X PUT -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/jassu2017/maven-project/contents/maven-project/blob/master/azure-pipelines.yml -d '{"message":"message","content":"Hi ..This is a tesing of updating a file from repo diptach event"}'| json_pp
+curl -X PUT -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/jassu2017/maven-project/contents/tree/master/webapp/abc.t -d '{"message":"message","content":"Hi ..This is a tesing of updating a file from repo diptach event"}'| json_pp
