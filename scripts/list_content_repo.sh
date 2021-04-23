71867e6ffec5e802ace60329ca867fa35ec77539
jassu2017 <jayshree.prasad@live.com>
2021-04-23 13:19:37 +0530

    updating a file in other repo using repo dispatch--removed README.md

--- scripts/list_content_repo.sh
+++ scripts/list_content_repo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X PUT -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/jassu2017/maven-project/contents/README.md -d '{"message":"updating the README.md", "content":"Hi ..This is a tesing of updating a file from repo diptach event"}'| json_pp
+curl -X PUT -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/jassu2017/maven-project/contents/webapp/hi.txt -d '{"message":"updating the README.md", "content":"Hi ..This is a tesing of updating a file from repo diptach event"}'| json_pp
